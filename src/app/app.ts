8b7876b939ec772971368b8600d24795abf89bec
SUPARNA DHARA <dhara.sup@gmail.com>
2025-11-22 22:21:50 +0530

    basic skeleton done

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('client');
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected title = 'client';
+  protected members = signal<any>([]);
+
+  /*ngOnInit(): void {
+    this.http.get('https://localhost:7215/api/members').subscribe({
+      next: response => this.members.set(response),
+      error: error => console.log(error),
+      complete: () => console.log('completed the http request') 
+    })
+  }*/
+
+    async ngOnInit() {
+      this.members.set(await this.getMembers())
+    }
+
+    async getMembers() {
+      try {
+        return lastValueFrom(this.http.get('https://localhost:7215/api/members'));
+      } catch (error) {
+        console.log(error);
+        throw error;
+      }
+    }
 }
