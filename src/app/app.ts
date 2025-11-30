fb2ec2970838d8af0822973aa7136bf4409a5de9
SUPARNA DHARA <dhara.sup@gmail.com>
2025-12-01 02:29:19 +0530

    Added basic login functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
+import { Nav } from "../layout/nav/nav";
+import { User } from '../types/user';
+import { AccountService } from '../core/services/account-service';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit{
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'client';
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([])
 
   /*ngOnInit(): void {
     this.http.get('https://localhost:7215/api/members').subscribe({
@@ -22,15 +26,22 @@ export class App implements OnInit{
   }*/
 
     async ngOnInit() {
-      this.members.set(await this.getMembers())
+      this.members.set(await this.getMembers());
+      this.setCurrentUser();
     }
+    setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
 
     async getMembers() {
-      try {
-        return lastValueFrom(this.http.get('https://localhost:7215/api/members'));
-      } catch (error) {
-        console.log(error);
-        throw error;
+    try {
+      return lastValueFrom(this.http.get<User[]>('https://localhost:7215/api/members'));
+    } catch (error) {
+      console.log(error);
+      throw error;
       }
     }
 }
